4ee501362dd472b78a9e2c3f6b4896e082a29fca
hercules-sousa <herculesdeso@gmail.com>
2020-12-26 18:00:04 -0300

    feat: image covering whole page

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 
 import "./styles.css"
-import Background from "../../assets/images/background.png"
 
 const Landing = () => {
   return (
     <div id="container">
-      <img src={Background} alt="Background"/>
     </div>
   )
 }
