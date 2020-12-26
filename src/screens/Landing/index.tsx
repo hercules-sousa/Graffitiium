5877a6f132a57d444238b5fc4012c17855b25c92
hercules-sousa <herculesdeso@gmail.com>
2020-12-26 15:45:11 -0300

    feat: adding assets

---
+++ src/screens/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./styles.css"
+import Background from "../../assets/images/background.png"
+
+const Landing = () => {
+  return (
+    <div id="container">
+      <img src={Background} alt="Background"/>
+    </div>
+  )
+}
+
+export default Landing
